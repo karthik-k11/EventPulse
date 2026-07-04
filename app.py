e5d05c177e6d02f5d7d190b531a1acfe2b48bda5
Karthik <karthikk1162@gmail.com>
2026-07-04 22:39:40 +0530

    Integrated the activity log with the Flask application and passed the detected events to the user interface.

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from flask import Flask, render_template, request
+from monitor import start_monitoring, stop_monitoring, activity_log
+import os
 
 app = Flask(__name__)
 
 selected_folder = ""
+monitoring = False
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     global selected_folder
+    global monitoring
 
     if request.method == "POST":
-        selected_folder = request.form.get("folder_path", "").strip()
+
+        action = request.form.get("action")
+
+        if action == "save":
+            selected_folder = request.form.get("folder_path", "").strip()
+
+            selected_folder = selected_folder.strip('"')
+
+        elif action == "start":
+
+            if (
+                selected_folder
+                and os.path.isdir(selected_folder)
+                and not monitoring
+            ):
+                start_monitoring(selected_folder)
+                monitoring = True
+
+        elif action == "stop":
+
+            if monitoring:
+                stop_monitoring()
+                monitoring = False
 
     return render_template(
         "index.html",
-        selected_folder=selected_folder
+        selected_folder=selected_folder,
+        monitoring=monitoring,
+        activity_log=activity_log
     )
 
 
